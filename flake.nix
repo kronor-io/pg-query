d7aec97eb9939355f3f786a3727be58556cd06f9
rhizomic <rhzmc@proton.me>
2024-11-25 16:02:12 -0500

    Format flake.nix with alejandra

--- flake.nix
+++ flake.nix
@@ -11,91 +11,101 @@
   };
 
   outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgsWithoutOverlay = import inputs.nixpkgs { inherit system; };
+    inputs.flake-utils.lib.eachDefaultSystem (system: let
+      pkgsWithoutOverlay = import inputs.nixpkgs {inherit system;};
 
-        ## C library for accessing the PostgreSQL parser outside of the
-        ## server environment
-        pg_query = pkgsWithoutOverlay.stdenv.mkDerivation {
-          name = "pg_query";
-          src = pkgsWithoutOverlay.fetchFromGitHub {
-            owner = "pganalyze";
-            repo = "libpg_query";
-            rev = "1ec38940e5c6f09a4c1d17a46d839a881c4f2db7";
-            sha256 = "X48wjKdgkAc4wUubQ5ip1zZYiCKzQJyQTgGvO/pOY3I=";
-          };
-          buildInputs = with pkgsWithoutOverlay; [ clang which protobufc ] ;
-          patchPhase = with pkgsWithoutOverlay; ''
+      # C library for accessing the PostgreSQL parser outside of the
+      # server environment
+      pg_query = pkgsWithoutOverlay.stdenv.mkDerivation {
+        name = "pg_query";
+        src = pkgsWithoutOverlay.fetchFromGitHub {
+          owner = "pganalyze";
+          repo = "libpg_query";
+          rev = "1ec38940e5c6f09a4c1d17a46d839a881c4f2db7";
+          sha256 = "X48wjKdgkAc4wUubQ5ip1zZYiCKzQJyQTgGvO/pOY3I=";
+        };
+        buildInputs = with pkgsWithoutOverlay; [clang which protobufc];
+        patchPhase = with pkgsWithoutOverlay;
+          ''
             ${gnused}/bin/sed -i.bak 's/prefix = .*/prefix = $(out)/g' Makefile
-          '' + pkgsWithoutOverlay.lib.optionalString pkgsWithoutOverlay.stdenv.isDarwin ''
+          ''
+          + pkgsWithoutOverlay.lib.optionalString pkgsWithoutOverlay.stdenv.isDarwin ''
             ${gnused}/bin/sed -i.bak 's/-Wl,-soname,$(SONAME)/-Wl/g' Makefile
           '';
-        };
+      };
 
-        overlay = final: prev: {
-          haskell = prev.haskell // {
-            packageOverrides = inputs.nixpkgs.lib.composeExtensions prev.haskell.packageOverrides
+      overlay = final: prev: {
+        haskell =
+          prev.haskell
+          // {
+            packageOverrides =
+              inputs.nixpkgs.lib.composeExtensions prev.haskell.packageOverrides
               (hfinal: hprev: {
-                pg-query = final.haskell.lib.appendConfigureFlags
-                  (hfinal.callCabal2nix "pg-query" ./. { inherit pg_query; })
+                pg-query =
+                  final.haskell.lib.appendConfigureFlags
+                  (hfinal.callCabal2nix "pg-query" ./. {inherit pg_query;})
                   [
                     "--extra-include-dirs=${pg_query}/include"
                     "--extra-lib-dirs=${pg_query}/lib"
                   ];
               });
           };
-          pg-query = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.pg-query;
-        };
-        pkgs = import inputs.nixpkgs { inherit system; overlays = [overlay]; };
-        unstablePkgs = import inputs.nixpkgs-unstable { inherit system; overlays = [ overlay ]; };
-        hspkgs = unstablePkgs.haskellPackages;
+        pg-query = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.pg-query;
+      };
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [overlay];
+      };
+      unstablePkgs = import inputs.nixpkgs-unstable {
+        inherit system;
+        overlays = [overlay];
+      };
+      hspkgs = unstablePkgs.haskellPackages;
+    in {
+      devShell = pkgs.haskell.packages.ghc98.shellFor {
+        withHoogle = true;
+        packages = p: [p.pg-query];
+        buildInputs = [
+          # Shell
 
-      in {
-            devShell = pkgs.haskell.packages.ghc98.shellFor rec {
-              withHoogle = true;
-              packages = p: [ p.pg-query ];
-              buildInputs = [
-                # Shell
-
-                ## Bash (for interactive use)
-                pkgs.bashInteractive
-
-
-                # Haskell Tooling
-
-                ## CLI interface for Cabal
-                hspkgs.cabal-install
-
-                ## Haskell formatter
-                hspkgs.fourmolu
-
-                ## HLS
-                (pkgs.haskell-language-server.override { supportedGhcVersions = [ "982" ]; })
-
-                ## HLint
-                hspkgs.hlint
-
-                ## hpack (to avoid needing to generate .cabal files)
-                hspkgs.hpack
+          ## Bash (for interactive use)
+          pkgs.bashInteractive
 
 
-                # Dev UX
+          # Haskell Tooling
 
-                ## https://pre-commit.com/
-                pkgs.pre-commit
+          ## CLI interface for Cabal
+          hspkgs.cabal-install
+
+          ## Haskell formatter
+          hspkgs.fourmolu
+
+          ## HLS
+          (pkgs.haskell-language-server.override {supportedGhcVersions = ["982"];})
+
+          ## HLint
+          hspkgs.hlint
+
+          ## hpack (to avoid needing to generate .cabal files)
+          hspkgs.hpack
 
 
-                # Protocol buffers
+          # Dev UX
 
-                ## Necessary for the protoc binary
-                unstablePkgs.protobuf
+          ## https://pre-commit.com/
+          pkgs.pre-commit
 
-                ## Necessary for the proto-lens-protoc binary
-                hspkgs.proto-lens-protoc
-              ];
-            };
 
-            packages.default = pkgs.haskellPackages.pg-query;
-          });
+          # Protocol buffers
+
+          ## Necessary for the protoc binary
+          unstablePkgs.protobuf
+
+          ## Necessary for the proto-lens-protoc binary
+          hspkgs.proto-lens-protoc
+        ];
+      };
+
+      packages.default = pkgs.haskellPackages.pg-query;
+    });
 }
